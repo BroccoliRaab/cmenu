92bda7d4a16636a60720a96e8dff469d9e3e2028
Robert Herlihy <rh828@tux2.cci.drexel.edu>
2022-02-10 10:03:16 -0500

    Make menu scrollable

--- cmenu.c
+++ cmenu.c
@@ -65,7 +65,7 @@ int main(){
 		}
 
 		selection_index = selection_index<0?0:selection_index;
-		selection_index = selection_index>getmaxy(stdscr)?getmaxy(stdscr):selection_index;
+		selection_index = selection_index>_items?n_items:selection_index;
 
 		padline = (selection_index/getmaxy(stdscr))*getmaxy(stdscr);
 		if (padline != previous_padline){
